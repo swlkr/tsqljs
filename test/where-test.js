aa1f32278f3a5e66362edc3896f94daabf503513
Sean Walker <swlkr.rbl@gmail.com>
2014-12-01 13:56:33 -0800

    Add where test

---
+++ test/where-test.js
@@ -0,0 +1,15 @@
+var chai = require('chai'),
+    expect = chai.expect,
+    sql = require('../lib/tsql');
+
+describe('where', function() {
+  it('should generate the correct sql statement', function() {
+    expect(sql.select().from('users').where('id = ?', 1).toQuery())
+      .to.deep.equal({text: 'select * from users where id = @1', values: [1]});
+  });
+
+  it('should handle multiple values', function() {
+    expect(sql.select().from('users').where('firstName = ? and lastName = ?', 'Johnny', 'Appleseed').toQuery())
+      .to.deep.equal({text: 'select * from users where firstName = @1 and lastName = @2', values: ['Johnny', 'Appleseed']});
+  });
+});
